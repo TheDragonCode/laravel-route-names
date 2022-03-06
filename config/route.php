117e75a7aa2dad26cca1a28dcd5bf157d6fc6c27
Andrey Helldar <helldar@ai-rus.com>
2022-03-06 15:32:24 +0300

    Update config

--- config/route.php
+++ config/route.php
@@ -3,13 +3,35 @@
 declare(strict_types=1);
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Names
+    |--------------------------------------------------------------------------
+    |
+    | This option determines the handling of route names.
+    |
+    */
+
     'names' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Exclude Names
+        |--------------------------------------------------------------------------
+        |
+        | This option specifies the names of the routes that will be excluded
+        | from the conversion.
+        |
+        */
+
         'exclude' => [
-            '_debugbar.*',
-            '_ignition.*',
-            'horizon.*',
-            'pretty-routes.*',
-            'telescope.*',
+            '__clockwork*',
+            '_debugbar*',
+            '_ignition*',
+            'horizon*',
+            'pretty-routes*',
+            'sanctum*',
+            'telescope*',
         ],
     ],
 ];
