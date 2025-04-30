a593d7201a2f7e714f544848936660e436886d2d
Andrey Helldar <helldar@dragon-code.pro>
2025-04-30 23:24:13 +0300

    Small refactoring for `RouteNameExtender.php`

--- tests/Extenders/RouteNameExtender.php
+++ tests/Extenders/RouteNameExtender.php
@@ -7,12 +7,20 @@ namespace Tests\Extenders;
 use DragonCode\LaravelRouteNames\Routing\Route;
 use Illuminate\Support\Str;
 
-final class RouteNameExtender
+class RouteNameExtender
 {
     public function __invoke(string $name, Route $route): string
     {
-        return Str::of($route->uri())->startsWith('api/v1')
-            ? Str::replaceFirst('api.v1', 'api.v2', $name)
-            : $name;
+        return $this->has($route->uri()) ? $this->replace($name) : $name;
+    }
+
+    protected function has(string $uri): bool
+    {
+        return Str::of($uri)->startsWith('api/v1');
+    }
+
+    protected function replace(string $name): string
+    {
+        return Str::replaceFirst('api.v1', 'api.v2', $name);
     }
 }
