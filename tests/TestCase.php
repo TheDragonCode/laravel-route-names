3f34d617206abc406bf5e994bf75a9c24d096d66
Andrey Helldar <helldar@dragon-code.pro>
2025-04-30 23:25:39 +0300

    Removed copyright

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the "dragon-code/support" project.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Andrey Helldar <helldar@ai-rus.com>
- *
- * @copyright 2021 Andrey Helldar
- *
- * @license MIT
- *
- * @see https://github.com/TheDragonCode/support
- */
-
 namespace Tests;
 
 use Illuminate\Support\Facades\Route;
